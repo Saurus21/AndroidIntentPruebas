8c0d93c2c837f245440a58a033b1a8ab6540c77c
Saurus21 <manuel.linconir.h@gmail.com>
2025-09-12 23:11:20 -0300

    Cambios al manifest y a AguaRuralApi.java

--- app/src/main/java/com/zebra/basicintent1/api/AguaRuralApi.java
+++ app/src/main/java/com/zebra/basicintent1/api/AguaRuralApi.java
@@ -3,7 +3,6 @@ package com.zebra.basicintent1.api;
 import com.zebra.basicintent1.modelosDatos.Lectura;
 import com.zebra.basicintent1.modelosDatos.Ruta;
 
-import java.math.BigInteger;
 import java.util.List;
 
 import retrofit2.http.*;
